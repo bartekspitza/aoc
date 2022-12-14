c3697d3f669d68e65dd97eacc29364ed7f348358
baspi <bartekspitza@hotmail.com>
2022-12-14 19:25:32 +0100

    adjust for part 2

--- 2/main.py
+++ 2/main.py
@@ -2,6 +2,33 @@ with open ('input.txt', 'r') as f:
 
     # X: Rock Y: Paper Z: Sciccors 
     # Win draw loss
+    # x=loose, y=draw, z=win
+    results = {
+        'A': ['C', 'A', 'B'],
+        'B': ['A', 'B', 'C'],
+        'C': ['B', 'C', 'A'],
+        }
+    outcomes = ['X','Y','Z']
+    asdf = ['A','B','C']
+    scores = []
+    score = 0
+    for line in f.readlines():
+        line = line.strip().split(' ')
+        him = line[0]
+        outcome = line[1]
+        what_to_play = results[him][outcomes.index(outcome)]
+
+        score += 3*outcomes.index(outcome)
+        score += asdf.index(what_to_play) + 1
+    
+    print(score)
+
+# Part 1
+"""
+with open ('input.txt', 'r') as f:
+
+    # X: Rock Y: Paper Z: Sciccors 
+    # Win draw loss
     asdf = {
         'X': ['C', 'A', 'B'],
         'Y': ['A', 'B', 'C'],
@@ -25,3 +52,4 @@ with open ('input.txt', 'r') as f:
         if me == 'Z':score+=3
     
     print(score)
+"""
